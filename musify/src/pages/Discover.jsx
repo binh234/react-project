d90346aee4b8ae93ac6d0798eed1c1cf82122c92
Binh Le <binhnd234@gmail.com>
2023-02-25 14:46:15 +0700

    Discover page in Musify"

--- musify/src/pages/Discover.jsx
+++ musify/src/pages/Discover.jsx
@@ -1,3 +1,27 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+
+const Discover = () => {
+  const genreTitle = 'Pop';
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
+        <select
+          onChange={() => { }}
+          value=""
+          className="bg-black text-gray-300 p-3 text-small rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
+        </select>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((song, idx) => (
+          <SongCard key={idx} song={song} i={idx} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Discover;
