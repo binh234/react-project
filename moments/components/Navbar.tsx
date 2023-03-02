ddf1f5caeb151c1a8ec106f20fa3561d52ebc0eb
Binh Le <binhnd234@gmail.com>
2023-03-02 21:11:26 +0700

    Setup Tailwin CSS

---
+++ moments/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
+
+import Logo from '@/utils/tiktik-logo.png'
+
+const Navbar = () => {
+  return (
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[130px]">
+          <Image className="cursor-pointer" src={Logo} alt="tiktik" />
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Navbar
--- moments/pages/_app.tsx
+++ moments/pages/_app.tsx
@@ -1,6 +1,29 @@
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false)
+  }, [])
+
+  if (isSSR) return null;
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-92vh overflow-hidden xl:hover:overflow-auto">
+          <Sidebar />
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  )
 }
