1f16984b5b86338066f33d0713580a15016f0d9a
Binh Le <binhnd234@gmail.com>
2023-03-04 20:02:28 +0700

    Start builing portfolio with next.js and styled-components

---
+++ next-portfolio/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '@/styles/GlobalComponents';
+import Button from '@/styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Do minim consequat adipisicing sunt. Sit aute ipsum aliqua veniam Lorem adipisicing adipisicing ad ex ipsum reprehenderit. Quis qui non pariatur dolor enim.
+      </SectionText>
+      <Button onClick={() => {}}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
---
+++ next-portfolio/header.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { DiCssdeck } from 'react-icons/di';
+
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+
+const Header = () =>  (
+  <Container>
+    <Div1>
+      <Link href="/">
+        <DiCssdeck size="3rem" /> <span>Portfolio</span>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+    </Div2>
+  </Container>
+);
+
+export default Header;
