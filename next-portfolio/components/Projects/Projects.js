bc70ceb8e0ade407992300c2df0bafcc661f3bd1
Binh Le <binhnd234@gmail.com>
2023-03-04 20:28:55 +0700

    Finish project and technologies section

--- next-portfolio/components/Projects/Projects.js
+++ next-portfolio/components/Projects/Projects.js
@@ -5,9 +5,34 @@ import { Section, SectionDivider, SectionTitle } from '@/styles/GlobalComponents
 import { projects } from '@/constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map((project, idx) => (
+        <BlogCard key={idx}>
+          <Img src={project.image} />
+          <TitleContent>
+            <HeaderThree title="true">{project.title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{project.description}</CardInfo>
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {project.tags.map((tag, idx) => (
+                <Tag key={idx}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+          <UtilityList>
+            <ExternalLinks href={project.visit}>View</ExternalLinks>
+            <ExternalLinks href={project.source}>Source</ExternalLinks>
+          </UtilityList>
+        </BlogCard>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
--- next-portfolio/components/Technologies/Technologies.js
+++ next-portfolio/components/Technologies/Technologies.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '@/styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "@/styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id='tech'>
+    <br />
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      Cillum officia cupidatat dolor ad ad esse in dolore in enim. In pariatur
+      elit Lorem irure ullamco amet sint mollit ullamco. Aliqua ea enim ut irure
+      pariatur ullamco proident tempor eiusmod in ut sint cupidatat.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>Front-End</ListContainer>
+        <ListParagraph>
+          Experience with <br />
+          React.js
+        </ListParagraph>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>Back-End</ListContainer>
+        <ListParagraph>
+          Experience with <br />
+          Node.js <br />
+          Go <br />
+        </ListParagraph>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>UI/UX</ListContainer>
+        <ListParagraph>
+          Experience with <br />
+          Figma
+        </ListParagraph>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
