af83bafb98b2ecf5f065879bce68dc8985ef9947
Binh Le <binhnd234@gmail.com>
2023-03-24 21:29:17 +0700

    Update hello API

--- moments/pages/api/hello.ts
+++ moments/pages/api/hello.ts
@@ -3,15 +3,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './auth/[...nextauth]'
 
-type Data = {
-  message: string
-}
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const session = await getServerSession(req, res, authOptions)
   if (session) {
     res.send({
-      message: 'This is protected content. You can access this content because you are signed in.',
+      version: "1.0",
+      appName: "Moments",
+      author: "Binh Le",
+      message: 'Congratulations! You are now authenticated^^',
     })
   } else {
     res.send({
