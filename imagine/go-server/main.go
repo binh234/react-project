e1b71ffce62413a0f38284c9c10ff937b0543d90
Binh Le <binhnd234@gmail.com>
2023-03-30 15:17:05 +0700

    Disconnect db after use

--- imagine/go-server/main.go
+++ imagine/go-server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"imagine/mongodb"
 	"imagine/routes"
 	"log"
 
@@ -12,14 +13,9 @@ func main() {
 	app := fiber.New()
 	// Default config for CORS
 	app.Use(cors.New())
-
-	// Or extend your config for customization
-	// app.Use(cors.New(cors.Config{
-	// 	AllowOrigins: "https://gofiber.io, https://gofiber.net",
-	// 	AllowHeaders: "Origin, Content-Type, Accept",
-	// }))
 	routes.RegisterDallERoutes(app)
 	routes.RegisterPostRoutes(app)
 
 	log.Fatal(app.Listen(":8080"))
+	defer mongodb.GetMongoInstance().Disconnect()
 }
