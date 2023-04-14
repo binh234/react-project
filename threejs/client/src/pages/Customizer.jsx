d5cfcb09b1b7f95ccadfe28a1f5c51c879a741ab
Binh Le <binhnd234@gmail.com>
2023-04-14 19:15:20 +0700

    Add customizer page

--- threejs/client/src/pages/Customizer.jsx
+++ threejs/client/src/pages/Customizer.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import state from '../store';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import config from '../config/config';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { useSnapshot } from 'valtio';
+import { CustomButton, Tab } from '../components';
 
 const Customizer = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>Customizer</div>
-  )
-}
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation('left')}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+          <motion.div className="absolute z-10 top-5 right-5" {...fadeAnimation}>
+            <CustomButton
+              type="filled"
+              title="Go back"
+              handleClick={() => (state.intro = true)}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
+          <motion.div className="filtertabs-container" {...slideAnimation('up')}>
+            {FilterTabs.map((tab) => (
+              <Tab key={tab.name} tab={tab} isFilterTab isActiveTab="" handleClick={() => {}} />
+            ))}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
+};
 
-export default Customizer
+export default Customizer;
