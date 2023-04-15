2bbbb1b2a7b489ca18a0147fcaa54e32a12f3e4f
Binh Le <binhnd234@gmail.com>
2023-04-15 10:15:17 +0700

    Setup express server

---
+++ threejs/server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit: "50mb"}))
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Hello World!'})
+})
