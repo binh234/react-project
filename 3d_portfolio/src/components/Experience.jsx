34acd25a86dd23f9194e526110ad20915632070e
Binh Le <binhnd234@gmail.com>
2023-06-29 16:07:02 +0700

    Add experience and tech section

--- 3d_portfolio/src/components/Experience.jsx
+++ 3d_portfolio/src/components/Experience.jsx
@@ -1,22 +1,56 @@
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
-import { motion } from "framer-motion"
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import { motion } from 'framer-motion'
 import 'react-vertical-timeline-component/style.min.css'
 
-import {styles} from '../styles'
-import {experiences} from '../constants'
-import {SectionWrapper} from '../hoc'
-import {textVariant} from '../utils/motion'
+import { styles } from '../styles'
+import { experiences } from '../constants'
+import { SectionWrapper } from '../hoc'
+import { textVariant } from '../utils/motion'
+
+const ExperienceCard = ({ title, company_name, icon, iconBg, date, points }) => {
+  <VerticalTimelineElement
+    contentStyle={{ background: '#1d1836', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid #232631' }}
+    date={date}
+    iconStyle={{ background: iconBg }}
+    icon={
+      <div className='flex justify-center items-center w-full h-full'>
+        <img src={icon} alt={company_name} className="w-3/5 h-3/5 object-contain" />
+      </div>
+    }
+  >
+    <div>
+      <h3 className='text-white text-2xl font-bold'>{title}</h3>
+      <p className='text-secondary text-sm font-semibold'>{company_name}</p>
+      <ul className='mt-4 list-disc ml-4 space-y-2'>
+        {points.map((point, idx) => (
+          <li key={idx} className='text-white-100 text-sm pl-1 tracking-wider'>
+            {point}
+          </li>
+        ))}
+      </ul>
+    </div>
+  </VerticalTimelineElement>
+}
 
 const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-
+        <p className={styles.sectionHeadText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience</h2>
       </motion.div>
+      <div className="mt-10 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, idx) => (
+            <ExperienceCard key={idx} {...experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
     </>
   )
 }
 
-const ExperienceWrapper = SectionWrapper(Experience, "experience")
+const ExperienceWrapper = SectionWrapper(Experience, 'experience')
 
 export default ExperienceWrapper
--- 3d_portfolio/src/components/Tech.jsx
+++ 3d_portfolio/src/components/Tech.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import {BallCanvas} from './canvas'
+import { SectionWrapper } from '../hoc'
+import { technologies } from '../constants'
 
 const Tech = () => {
-  return <div>Tech</div>
+  return (
+    <div className='grid lg:grid-cols-10 md:grid-cols-8 sm:grid-cols-6 xs:grid-cols-4 grid-cols-2 gap-6'>
+      {technologies.map((tech) => (
+        <div className='w-24 h-24' key={tech.name}>
+          <BallCanvas icon={tech.icon} />
+        </div>
+      ))}
+    </div>
+  )
 }
 
-export default Tech
+const TechWrapper = SectionWrapper(Tech, 'tech')
+
+export default TechWrapper
