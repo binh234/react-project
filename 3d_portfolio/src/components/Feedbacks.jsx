5ca116f3d4162b6cba12fbcddebae7055c625879
Binh Le <binhnd234@gmail.com>
2023-06-29 19:56:22 +0700

    Add testimonials section

--- 3d_portfolio/src/components/Feedbacks.jsx
+++ 3d_portfolio/src/components/Feedbacks.jsx
@@ -10,45 +10,38 @@ const FeedbackCard = ({ index, testimonial, name, designation, company, image })
       className="w-full bg-black-200 p-4 rounded-2xl"
       variants={fadeIn('right', 'spring', 0.3 * index, 0.75)}
     >
-      {/* <div className="relative w-full h-[230px]">
-        <img src={image} alt="thumbnail" className="w-full h-full object-contain rounded-2xl" />
-        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-          <div
-            onClick={() => window.open(source_code_link, '_blank')}
-            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-          >
-            <img src={github} alt="github" className="w-3/5 h-3/5 object-contain" />
+      <p className="text-white text-6xl font-bold">&quot;</p>
+      <div>
+        <p className="text-secondary text-lg tracking-wider">{testimonial}</p>
+        <div className="mt-4 flex flex-row justify-between items-center gap-1">
+          <div className="flex-1 flex flex-col gap-1">
+            <p className="text-white font-medium text-base">
+              <span className="blue-text-gradient">@</span> {name}
+            </p>
+            <p className='text-secondary text-xs'>
+              {designation} of {company}
+            </p>
           </div>
+          <img src={image} alt="photo" className='rounded-full w-12 h-12 object-cover' />
         </div>
       </div>
-      <div className="mt-4">
-        <h3 className="text-white text-lg font-bold">{name}</h3>
-        <p className="mt-2 text-secondary text-sm">{description}</p>
-      </div>
-      <div className="mt-4 flex flex-wrap gap-2">
-        {tags.map((tag, idx) => (
-          <p key={idx} className={`text-sm ${tag.color || 'text-white'}`}>
-            #{tag.name}
-          </p>
-        ))}
-      </div> */}
     </motion.div>
   )
 }
 
 const Feedbacks = () => {
   return (
-    <>
+    <div className=' bg-tertiary rounded-2xl px-4 py-6'>
       <motion.div variants={textVariant()}>
         <p className={styles.sectionSubText}>What others say</p>
         <h2 className={styles.sectionHeadText}>Testinomials</h2>
       </motion.div>
-      <div className="mt-10 flex flex-wrap gap-7">
+      <div className="mt-10 grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
         {testimonials.map((testimonial, idx) => (
           <FeedbackCard key={idx} index={idx} {...testimonial} />
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
