65326ee0da5449c69cc08f9d9620bbb28eaa7f0c
Binh Le <binhnd234@gmail.com>
2023-06-29 11:37:56 +0700

    Refactor constants

---
+++ 3d_portfolio/src/constants/services.js
@@ -0,0 +1,20 @@
+import { web, mobile, backend, creator } from '../assets'
+
+export const services = [
+  {
+    title: 'Web Developer',
+    icon: web,
+  },
+  {
+    title: 'React Native Developer',
+    icon: mobile,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'Content Creator',
+    icon: creator,
+  },
+]
